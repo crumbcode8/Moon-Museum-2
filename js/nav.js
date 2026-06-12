7abb3911256d3afea8b270413c7741f1cc49f635
crumbcode8 <audreycooper17@gmail.com>
2026-06-12 12:30:18 -0500

    Update nav.js
    updated styling

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
-if (hamburger && navLinks) {
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-  });
-}
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
